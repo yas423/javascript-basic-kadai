8ec92a34f04016ffe14e3a5ccd829c3872bf4fb1
yas423 <yuasa@hodumi.com>
2024-08-29 15:41:18 +0900

    課題提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+// 体重：68kg
+// 身長：1.7m
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+
+// 変数の宣言・値の代入
+let userWeight=68;
+let userHight=1.7;
+
+// コンソールへの出力
+console.log( userWeight / ( userHight * userHight ) );
